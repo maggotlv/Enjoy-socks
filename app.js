87102b837c4cc4636dc9d5c3d8471e26206813b5
maggotlv <v.voicehovich@yandex.ru>
2023-05-23 11:45:32 +0300

    Dev branch opened

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: false, // false - только для http, true - только для https
+      secure: false,
       maxAge: 1000 * 60 * 60 * 24 * 2,
       httpOnly: true,
     },
