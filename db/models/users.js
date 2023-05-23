98ebbb79e1babf1b1f9526f5fc2a35317508ecb5
forjmail <forj@mail.ru>
2023-05-23 15:09:20 +0400

    DB

--- db/models/users.js
+++ db/models/users.js
@@ -1,25 +1,23 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class Users extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      this.hasMany(models.Socks, { foreignKey: 'author' });
+      this.hasMany(models.Favorites, { foreignKey: 'user' });
+      this.hasMany(models.Carts, { foreignKey: 'user' });
     }
   }
-  Users.init({
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Users',
-  });
+  Users.init(
+    {
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: 'Users',
+    }
+  );
   return Users;
 };
