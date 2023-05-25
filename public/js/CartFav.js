55153b4e3310486cbf3108d169837f3f39f6f0fd
maggotlv <v.voicehovich@yandex.ru>
2023-05-25 14:56:29 +0300

    Generator 0.99

--- public/js/CartFav.js
+++ public/js/CartFav.js
@@ -1,11 +1,12 @@
 const addFav = document.querySelector('#addFav');
 const addCart = document.querySelector('#addCart');
 
+
 addFav?.addEventListener('click', async () => {
+  const colour = sockBckg.style.background;
+  const pattern = sockPattern.src.slice(35, 55);
+  const image = sockLogo.src.slice(33, 55);
   try {
-    const colour = sockBckg.style.background;
-    const pattern = sockPattern.src.slice(35, 55);
-    const image = sockLogo.src.slice(33, 55);
     const sockData = { colour, pattern, image };
     const response = await fetch('socks/addFav', {
       method: 'POST',
@@ -13,7 +14,25 @@ addFav?.addEventListener('click', async () => {
       body: JSON.stringify({ sockData }),
     });
     const result = await response.json();
-    console.log(result);
+    console.log(result.author);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+addCart?.addEventListener('click', async () => {
+  const colour = sockBckg.style.background;
+  const pattern = sockPattern.src.slice(35, 55);
+  const image = sockLogo.src.slice(33, 55);
+  try {
+    const sockData = { colour, pattern, image };
+    const response = await fetch('socks/addCart', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ sockData }),
+    });
+    const result = await response.json();
+    console.log(result.author);
   } catch (error) {
     console.log(error);
   }
--- src/routes/socks.routes.js
+++ src/routes/socks.routes.js
@@ -13,20 +13,19 @@ const trans = {
   'rgb(255, 139, 134)': 'Розовый',
   'rgb(218, 255, 134)': 'Салатовый',
   'rgb(134, 244, 255)': 'Голубой',
-  'cactus.png': 'кактус',
-  'duck.png': 'уточка',
-  'llama.png': 'лама',
+  'cactus.png': 'Кактус',
+  'duck.png': 'Уточка',
+  'llama.png': 'Лама',
   'yoda.png': 'Йода',
   'gorokhoviy.png': 'гороховый',
   'nauchniy.png': 'научный',
   'sovremenniy.png': 'современный',
-  'zvezdniy.png': 'звёздный'
+  'zvezdniy.png': 'звёздный',
 };
 
 router.get('/', (req, res) => {
   res.render(Generator, {});
 });
-
 
 router.post('/addFav', async (req, res) => {
   try {
@@ -36,9 +35,34 @@ router.post('/addFav', async (req, res) => {
       pattern: sockData.pattern,
       image: sockData.image,
       author: req.session.user.id,
-      sockname: `${trans[sockData.colour]} ${trans[sockData.pattern]} ${trans[sockData.image]}`
+      sockname: `${trans[sockData.colour]} ${trans[sockData.pattern]} носочек ${trans[sockData.image]}`,
     });
-    res.json('Sock added');
+    const favouriteSock = await Favorites.create({
+      user: createSock.author,
+      sock: createSock.id,
+    });
+    res.json(createSock);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.post('/addCart', async (req, res) => {
+  try {
+    const sockData = await req.body.sockData;
+    const createSock = await Socks.create({
+      colour: sockData.colour,
+      pattern: sockData.pattern,
+      image: sockData.image,
+      author: req.session.user.id,
+      sockname: `${trans[sockData.colour]} ${trans[sockData.pattern]} носочек ${trans[sockData.image]}`,
+    });
+    const cartSock = await Carts.create({
+      user: createSock.author,
+      sock: createSock.id,
+      count: 1,
+    });
+    res.json(createSock);
   } catch (error) {
     console.log(error);
   }
